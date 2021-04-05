ecc8e7eebca1952d0ca25c9685d06236ff531421
Jonas Mosmann <jonasmosmann@web.de>
2021-04-05 16:41:23 +0200

    queryselector hinzugefügt

---
+++ src/Queryselectors.ts
@@ -0,0 +1,5 @@
+export const cup1 = document.querySelector ("#cup-1") as HTMLInputElement;
+export const cup2 = document.querySelector ("#cup-2") as HTMLInputElement;
+export const cup3 = document.querySelector ("#cup-3") as HTMLInputElement;
+export const ball = document.querySelector ("#ball") as HTMLInputElement;
+export const startButton = document.querySelector ("#start-button") as HTMLInputElement;
