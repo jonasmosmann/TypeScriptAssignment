0132ae3f26f0cf8dc62f23cf0c20b7caf8edef89
Jonas Mosmann <jonasmosmann@web.de>
2021-04-05 16:22:25 +0200

    ts file names geändert

--- src/unusedModule.ts
+++ src/unusedModule.ts
@@ -4,3 +4,32 @@
 function unused() {
   return "hugh, why was this neccesary";
 }
+
+console.log(helloWorld);
+customElements.define("my-beispiel", Beispiel);
+
+alertMe();
+
+const myInputValue = document.querySelector<HTMLInputElement>("#myInput");
+
+const myInputValueAlternate = document.querySelector(
+  "#myInput"
+) as HTMLInputElement;
+
+document
+  .querySelector<HTMLInputElement>("#myInput")
+  ?.addEventListener("focus", doSmth);
+
+function doSmth(e: UIEvent) {
+  const val = e.target as HTMLInputElement;
+  console.log(e, val.value);
+}
+
+export const helloWorld: string = "Hello world";
+
+export class Beispiel extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = "<h1>Hello World</h1>";
+  }
+}
