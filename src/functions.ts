7d2f7d95b13a58ec552a685a244f2eb9a3fc80d3
Jonas Mosmann <jonasmosmann@web.de>
2021-04-19 13:47:09 +0200

    gameStart animation erstellt(Basis)

--- src/functions.ts
+++ src/functions.ts
@@ -1,5 +1,15 @@
-import { cup2, startButton } from "./Queryselectors";
+import { ball, cup1, cup2, cup3, startButton } from "./Queryselectors";
 
-export function moveUp(e:UIEvent){
-    cup2.classList.add("animate");
+export function moveUp(e:UIEvent):void{
+    cup2.classList.add("animate-cup2");
     }
+
+    export function moveBall(e:UIEvent):void{
+       ball.style.top= "400px";}
+
+            export function moveCup1(e:UIEvent):void{
+                cup1.classList.add("animate-cup1");
+                }    
+                export function moveCup3(e:UIEvent):void{
+                    cup3.classList.add("animate-cup3");
+                    }   
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-import { moveUp } from "./functions";
+import { moveBall, moveCup1, moveCup3, moveUp } from "./functions";
 import { startButton } from "./Queryselectors";
+
+gameStart();
 
 function gameStart():void{
     startButton.addEventListener("click", moveUp);
+    startButton.addEventListener("click", moveBall);
+    startButton.addEventListener("click", moveCup1);
+    startButton.addEventListener("click", moveCup3);
 }
