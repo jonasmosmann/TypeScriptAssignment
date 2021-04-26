027e6bebe3fa6e8d433ed62dae80740d0d7b103e
Jonas Mosmann <jonasmosmann@web.de>
2021-04-26 20:11:01 +0200

    all-cups Class erstellt

--- src/Queryselectors.ts
+++ src/Queryselectors.ts
@@ -1,3 +1,4 @@
+export const allCups = document.querySelector (".all-cups") as HTMLDivElement;
 export const cup1 = document.querySelector ("#cup-1") as HTMLDivElement;
 export const cup2 = document.querySelector ("#cup-2") as HTMLDivElement;
 export const cup3 = document.querySelector ("#cup-3") as HTMLDivElement;
